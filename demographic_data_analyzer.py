3a9d560671077b1942044678df19d72465dc4a88
gomer110 <gomer_110@hotmail.com>
2024-09-15 23:01:20 -0400

    git commit -m"next"

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
@@ -13,12 +12,13 @@ def calculate_demographic_data(print_data=True):
     # print(df.info)
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    
-    # print(df['race'].value_counts())
 
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
+    
+    print(df['sex'].unique())
+    # print(df['sex'][df['sex'] == 'Male'].mean())
     average_age_men = None
 
     # What is the percentage of people who have a Bachelor's degree?
