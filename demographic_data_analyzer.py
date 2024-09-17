92f8c568402f4f81d4bb9e89f7894697c41c9479
gomer110 <gomer_110@hotmail.com>
2024-09-16 20:12:37 -0400

    git commit -m"done"

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -17,11 +17,11 @@ def calculate_demographic_data(print_data=True):
 
     # What is the average age of men?
 
-    print(df['age'][df['sex'] == 'Male'].mean())
     average_age_men = df['age'][df['sex'] == 'Male'].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    
+    percentage_bachelors = df['education'][df['education'] == 'Bachelors'].count() / df['education'].count()
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
